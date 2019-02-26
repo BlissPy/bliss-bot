c625c9b84fea02db4acc6caa1579fc5427a59246
Liam H <35095843+ir-3@users.noreply.github.com>
2019-02-26 14:22:08 -0600

    Trying to make a sobel

--- imageops/numpyops.py
+++ imageops/numpyops.py
@@ -4,16 +4,23 @@ from io import BytesIO
 import skimage
 import skimage.transform
 import numpy as np
+rom skimage import data
+from skimage.exposure import rescale_intensity
+from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
 from PIL import Image
 from skimage import io
 
 
 async def bytes_to_np(img_bytes: BytesIO):
+    """This takes a BytesIO containing an image and converts them to a np.ndarray using
+    skimage.io.imread."""
     ret = skimage.io.imread(img_bytes)
     return ret
 
 
 async def np_to_bytes(img_bytes: BytesIO):
+    """This takes a np.ndarray containing an image and converts it to a BytesIO object
+    containing an image."""
     pil = Image.fromarray(img_bytes)
     b = BytesIO()
     pil.save(b, format="png")
@@ -24,13 +31,16 @@ async def np_to_bytes(img_bytes: BytesIO):
 
 def _sort(img: np.ndarray):
     shape = img.shape
-
     img = img.reshape((img.shape[0] * img.shape[1], img.shape[2]))
+    
     img.sort(0)
 
     return img.reshape(shape)
 
-
+# If you are seeing this and think that you can make
+# the selection of characters better, please do.
+# this is just some random one I found online and
+# made into a dictionary.
 _ascii_characters = {
     0: " ", 1: ".", 2: "'", 3: "`",
     4: "^", 5: "\"", 6: ",", 7: ":",
@@ -49,11 +59,6 @@ _ascii_characters = {
 
 
 def _ascii_art(img: np.ndarray):
-    # this was normalization but idk if i like it
-    # img += int(255 / img.max())
-
-    print()
-
     ascii_art = ""
 
     for i_row in range(0, img.shape[0], 2):
@@ -65,3 +70,12 @@ def _ascii_art(img: np.ndarray):
             ascii_art += _ascii_characters[int(avg / 5)]
 
     return ascii_art
+
+
+@adapt_rgb(each_channel)
+def _sobel_each(image):
+    return filters.sobel(image)
+
+
+def _sobel(img: np.ndarray):
+    return rescale_intensity(1 - _sobel_each(img))
