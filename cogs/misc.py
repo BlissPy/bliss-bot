c1f8b3df6d92645e0ea84c541be3cdcc63203a04
Liam H <admin@ir3.me>
2019-02-27 18:07:54 -0600

    Removing test

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,3 @@
-import string
-import random
-
 from discord.ext import commands
 
 
@@ -12,13 +9,6 @@ class Miscellaneous(commands.Cog):
     @commands.command(name="ping")
     async def ping(self, ctx):
         await ctx.send(f"{ctx.author.mention}, 🏓 {round(self.bot.latency*1000, 2)}ms")
-
-    @commands.command(name="test", hidden=True)
-    @commands.is_owner()
-    async def test(self, ctx):
-        m = await ctx.send("_ _")
-        for _ in range(0, 15):
-            m = await m.edit(content=m.content + random.choice(string.printable))
 
 
 def setup(bot):
