800206a0b808c41ba69be871aa54f28bb80cb6f7
Liam H <admin@ir3.me>
2019-03-23 17:52:16 -0500

    Add SQL command

--- cogs/database.py
+++ cogs/database.py
@@ -1,7 +1,12 @@
-from aiofiles import open as async_open
+import time
+import io
 
 import asyncpg
+import discord
 from discord.ext import commands
+from aiofiles import open as async_open
+
+from utils.formatting import TabularData, Plural
 
 
 class Database(commands.Cog):
@@ -9,7 +14,6 @@ class Database(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.credentials = bot.db_credentials
-        self.db = None
         bot.loop.create_task(self.setup_database())
 
     async def setup_database(self):
@@ -17,13 +21,54 @@ class Database(commands.Cog):
         async with async_open("setup.sql") as sql:
             await db.execute(await sql.read())
 
-        self.db = db
         self.bot.db = db
 
-    @commands.command()
-    @commands.is_owner()
-    async def execute(self, ctx, *, sql: str):
-        await ctx.send("```\n" + await self.db.execute(sql) + "```")
+    @staticmethod
+    def cleanup_code(content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    # https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L208-L249
+    @commands.command(hidden=True)
+    async def sql(self, ctx, *, query: str):
+        """Run some SQL."""
+        query = self.cleanup_code(query)
+
+        is_multistatement = query.count(';') > 1
+        if is_multistatement:
+            # fetch does not support multiple statements
+            strategy = self.bot.db.execute
+        else:
+            strategy = self.bot.db.fetch
+
+        try:
+            start = time.perf_counter()
+            results = await strategy(query)
+            dt = (time.perf_counter() - start) * 1000.0
+        except Exception:
+            return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+
+        rows = len(results)
+        if is_multistatement or rows == 0:
+            return await ctx.send(f'`{dt:.2f}ms: {results}`')
+
+        headers = list(results[0].keys())
+        table = TabularData()
+        table.set_columns(headers)
+        table.add_rows(list(r.values()) for r in results)
+        render = table.render()
+
+        fmt = f'```\n{render}\n```\n*Returned {Plural(row=rows)} in {dt:.2f}ms*'
+        if len(fmt) > 2000:
+            fp = io.BytesIO(fmt.encode('utf-8'))
+            await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
+        else:
+            await ctx.send(fmt)
 
 
 def setup(bot):
