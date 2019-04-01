144654cda4401e539764a25e451e84a1752a8ea0
Liam H <admin@ir3.me>
2019-04-01 17:54:02 -0500

    BDO

--- bdo/cog.py
+++ bdo/cog.py
@@ -88,7 +88,7 @@ class BDOCog(commands.Cog, name="Bliss Desert Online"):
     async def location(self, ctx, location_name: str = None):
         if location_name is None:
             player = self.map.get_player(ctx.author.id)
-            location = player.location
+            location = await player.location
         else:
             location = self.map.get_location(location_name)
 
