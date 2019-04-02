f9ee0760fc64af3331a052e4367a27c1f487039f
Liam H <admin@ir3.me>
2019-04-02 18:48:53 -0500

    BDO

--- bdo/monsters.py
+++ bdo/monsters.py
@@ -1,16 +1,17 @@
-import random.randint
+import random
+
 
 class Monster:
-    
+
     def __init__(self, manager, **data):
         self.manager = manager
-        
+
         self.id = data.get("id")
         self.name = data.get("name")
         self.recommended_levels = data.get("recommended_levels")
         self.spawn_locations = [self.manager.get_location(loc) for loc in data.get("spawn_locations")]
         self.rarity = data.get("rarity")
-        
+
         self.min_exp = data.get("min_exp")
         self.max_exp = data.get("max_exp")
 
