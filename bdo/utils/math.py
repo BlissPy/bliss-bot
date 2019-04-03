ae7fd6ef50c1dfd1327d1550b709b172572399de
Liam H <35095843+ir-3@users.noreply.github.com>
2019-04-03 13:44:43 -0500

    Update math.py

--- bdo/utils/math.py
+++ bdo/utils/math.py
@@ -6,3 +6,6 @@ def exp_to_level(exp: int):
 
 def coord_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+def travel_time(distance: float):
+    return distance * 360
