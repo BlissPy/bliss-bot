80c8bea7e1e10410b61e0b0edffafa208df0e136
Liam H <35095843+ir-3@users.noreply.github.com>
2019-04-05 13:53:57 -0500

    Update redis.py

--- cogs/redis.py
+++ cogs/redis.py
@@ -1,4 +1,6 @@
+import time
 import asyncio
+import traceback
 
 import aioredis
 from discord.ext import commands
@@ -10,12 +12,24 @@ class Redis(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.redis = None
         bot.loop.create_task(self.setup_redis())
 
     async def setup_redis(self):
-        self.redis = await asyncio.wait_for(aioredis.create_pool(self.REDIS_ADDRESS), timeout=420.420)
-        self.bot.redis = self.redis
+        redis = await asyncio.wait_for(aioredis.create_pool(self.REDIS_ADDRESS), timeout=420.420)
+        self.bot.redis = redis
+        return redis
+        
+    @commands.command(name="redis")
+    @commands.is_owner()
+    async def redis(self, ctx, method: str, *redis_args):
+        try:
+            start = time.perf_counter()
+            out = await self.bot.redis.execute(method, *redis_args)
+            duration = (time.perf_counter() - start) * 1000.0
+        except:
+            return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+        
+        await ctx.send(f"*{round(duration, 2)}ms:* `{out}`")
 
 
 def setup(bot):
